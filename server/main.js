a3fc416bc37638d677ba2ae7e9a60e0bfb6bbfcc
Gökhan Açıkgöz <gokhanckgz@gmail.com>
2018-12-10 14:15:28 +0300

    cayley url changed

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ import '../imports/api/data';
 import '../imports/api/pitfall-scanner';
 import '../imports/api/rdf-translator';
 
-export const CAYLEY_URL = "http://localhost:64210/";
+export const CAYLEY_URL = "http://194.27.156.189:64210/";
 
 Meteor.startup(() => {
 
